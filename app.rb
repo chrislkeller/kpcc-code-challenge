9732c81fbfc4577097f3c230c2a681352d133601
Chris Keller <christopherlawrencekeller@gmail.com>
2017-02-04 14:17:44 -0800

    implements post route to take search term and query kpcc article api and parse response to hash

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'json'
+require 'open-uri'
+include ERB::Util
 
+set :public_folder, "public"
 set :views, "views"
 
 get '/' do
@@ -7,6 +11,11 @@ get '/' do
 end
 
 post '/search/' do
+    url_prefix = "http://www.scpr.org/api/v3/articles?types=news&query="
     searchterm = params[:searchterm] || "Nothing Submitted"
-    erb :search_results, :locals => {'searchterm' => searchterm}
+    encoded_searchterm = url_encode(searchterm)
+    request_url = "#{url_prefix}#{encoded_searchterm}"
+    request_data = open(request_url).read
+    response = JSON.parse(request_data)
+    erb :search_results, :locals => {'searchterm' => searchterm, 'articles' => response["articles"]}
 end
