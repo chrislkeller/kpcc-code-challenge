1b1eab4739433a00840d067640ef9760f0324fb9
Chris Keller <christopherlawrencekeller@gmail.com>
2017-02-04 12:49:11 -0800

    adds the start of the search route to the application

--- app.rb
+++ app.rb
@@ -5,3 +5,8 @@ set :views, "views"
 get '/' do
     erb :search_form
 end
+
+post '/search/' do
+    searchterm = params[:searchterm] || "Nothing Submitted"
+    erb :index, :locals => {'searchterm' => searchterm}
+end
