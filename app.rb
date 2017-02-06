e99d3461b83335dfc8168262b60703c63c989313
Chris Keller <christopherlawrencekeller@gmail.com>
2017-02-05 21:51:39 -0800

    spacing and blank lines in the app.rb adjusted

--- app.rb
+++ app.rb
@@ -12,22 +12,17 @@ end
 
 post '/search/' do
     url_prefix = "http://www.scpr.org/api/v3/articles?types=news&query="
-
     # needs to handle searchterm of null
     searchterm = params[:searchterm] || "Nothing Submitted"
     encoded_searchterm = url_encode(searchterm)
     request_url = "#{url_prefix}#{encoded_searchterm}"
-
     # needs to handle api response error
     request_data = open(request_url).read
     response = JSON.parse(request_data)
-
     quantity = response["articles"].length
-
     erb :search_results, :locals => {
         "searchterm" => searchterm,
         "articles" => response["articles"],
         "quantity" => quantity
     }
-
 end
